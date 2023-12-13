3d34de02a64e0cd163dc90dacc7a01b1dd61b90f
Cactus <115384394+Cactoidal@users.noreply.github.com>
2023-12-13 15:58:29 -0500

    Create Player.gd

---
+++ godot/Player.gd
@@ -0,0 +1,148 @@
+extends KinematicBody
+
+var ethers
+
+var speed = 7
+const ACCEL_DEFAULT = 7
+const ACCEL_AIR = 1
+onready var accel = ACCEL_DEFAULT
+var gravity = 9.8
+var jump = 5
+
+var cam_accel = 40
+var mouse_sense = 0.1
+var snap
+
+var direction = Vector3()
+var velocity = Vector3()
+var gravity_vec = Vector3()
+var movement = Vector3()
+
+var ui
+var tiles
+var current_tile
+var trap_phase = true
+var mine_phase = false
+
+var trapped_tiles = []
+
+var mine_wait_timer = 0
+
+var start_pos
+
+func _ready():
+	start_pos = global_transform.origin
+	ui = get_parent().get_node("UI")
+	tiles = get_parent().get_node("Plateau/Tiles")
+	current_tile = tiles.get_children()[0]
+	var tile_number = 0
+	for tile in tiles.get_children():
+		tile.tile_number = tile_number
+		tile_number += 1
+	
+	ui.get_node("TrapPhase/SetTraps").connect("pressed", self, "set_traps")
+	ui.get_node("TrapPhase/Revert").connect("pressed", self, "revert_traps")
+	ui.get_node("MinePhase/StopMining").connect("pressed", self, "stop_mining")
+		
+func _physics_process(delta):
+	if global_transform.origin.y < -20:
+		global_transform.origin = start_pos
+		gravity_vec[1] /= 2
+		
+	if mine_wait_timer > 0:
+		mine_wait_timer -= delta
+		ui.get_node("MinePhase/MineTimer").text = String(mine_wait_timer)
+		if mine_wait_timer < 0:
+			ui.get_node("MinePhase/MineTimer").visible = false
+			mine_wait_timer = 0
+	#get keyboard input
+	direction = Vector3.ZERO
+	var h_rot = global_transform.basis.get_euler().y
+	var f_input = Input.get_action_strength("down") - Input.get_action_strength("up")
+	var h_input = Input.get_action_strength("right") - Input.get_action_strength("left")
+	direction = Vector3(h_input, 0, f_input).rotated(Vector3.UP, h_rot).normalized()
+	
+	#jumping and gravity
+	if is_on_floor():
+		snap = -get_floor_normal()
+		accel = ACCEL_DEFAULT
+		gravity_vec = Vector3.ZERO
+	else:
+		snap = Vector3.DOWN
+		accel = ACCEL_AIR
+		gravity_vec += Vector3.DOWN * gravity * delta
+		
+#	if Input.is_action_just_pressed("jump") and is_on_floor():
+#		snap = Vector3.ZERO
+#		gravity_vec = Vector3.UP * jump
+
+	if Input.is_action_just_pressed("action"):
+		if trap_phase == true && trapped_tiles.size() < 3:
+			current_tile.set_trap()
+		elif mine_phase == true && mine_wait_timer == 0:
+			current_tile.try_mine()
+	
+	#make it move
+	velocity = velocity.linear_interpolate(direction * speed, accel * delta)
+	movement = velocity + gravity_vec
+	
+	move_and_slide_with_snap(movement, snap, Vector3.UP)
+	
+	
+func set_traps():
+	if trapped_tiles.size() == 3:
+		trap_phase = false
+		var trap1 = trapped_tiles[0].tile_number
+		var trap2 = trapped_tiles[1].tile_number
+		var trap3 = trapped_tiles[2].tile_number
+		
+		#turned off for now
+		#ethers.start_transaction("get_chain_public_key", [trap1,trap2,trap3])
+		
+		trapped_tiles = []
+		ethers.get_node("Fadeout/Background").visible = true
+		ethers.get_node("Fadeout/Background/Waiting").visible = true
+		ethers.fade("wait_for_mining")
+
+func start_mining():
+	ethers.get_node("Fadeout/Background/Waiting").visible = false
+	ethers.get_node("Fadeout/Background").visible = false
+	ui.get_node("TrapPhase").visible = false
+	mine_phase = true
+	ui.get_node("MinePhase").visible = true
+	for tile in tiles.get_children():
+		tile.already_used = false
+		tile.stay_visible = false
+		tile.visible = false
+		if tile == current_tile && tile.activated == true:
+			tile.visible = true
+
+func revert_traps():
+	for trap in trapped_tiles:
+		trap.stay_visible = false
+		trap.already_used = false
+		trap.visible = false
+		if trap == current_tile && trap.activated == true:
+			trap.visible = true
+	trapped_tiles = []
+	ui.get_node("TrapPhase/TrapsCount").text = "Traps Set:\n0 / 3"
+	
+		
+
+func stop_mining():
+	if mine_wait_timer == 0:
+		#send tx
+		mine_phase = false
+		ui.get_node("MinePhase").visible = false
+		ethers.get_node("Fadeout/Background").visible = true
+		ethers.get_node("Fadeout/Background/Waiting").visible = true
+		ethers.fade("wait_for_game_end")
+
+
+func conclude_game():
+	ethers.get_node("Fadeout/Background/Waiting").visible = false
+	ethers.get_node("Fadeout/Background").visible = false
+	ethers.camera.make_current()
+	ethers.fadein = true
+	ethers.in_queue = false
+	get_parent().queue_free()
